c3bb06cd5be17d90ead3a41c645f0638bb319fdd
Ricardo <ricardossmc@gmail.com>
2019-05-02 15:31:19 +0200

    Añadido proyecto

---
+++ Recetas/Recetas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
